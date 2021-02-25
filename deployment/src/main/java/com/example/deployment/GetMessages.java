83eb7b70d0cf590ca9da0ffa3b76979d84459481
vivek sarabu <vivek_sarabu@papajohns.com>
2021-02-25 15:35:20 -0500

    jenkins file added

--- deployment/src/main/java/com/example/deployment/GetMessages.java
+++ deployment/src/main/java/com/example/deployment/GetMessages.java
@@ -1,14 +1,11 @@
 package com.example.deployment;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 public class GetMessages{
 
-    @GetMapping("/{data}")
-    public String getGreeting(@PathVariable("data") String data)  {
-        return "Hi hello"+data;
+
+    public String getGreeting()  {
+        return "Hi hello Jenkins";
     }
 }
